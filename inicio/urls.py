1080bea6840562c178d014779fde50da255d94da
AzK <leandro.glc@gmail.com>
2023-11-25 21:19:32 -0300

    Ajustes endpoints para adicionar, visualizar, migracoes, templates, banco de dados e imagens para documentacao

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from inicio.views import index, imagem
+from inicio.views import index, imagem, lista_produtos, adicionar_produto
 
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('imagem/', imagem, name='imagem')
+    path("", index, name="index"),
+    path("imagem/", imagem, name="imagem"),
+    path("lista/", lista_produtos, name="lista_produtos"),
+    path("adicionar/", adicionar_produto, name="adicionar_produto"),
 ]
